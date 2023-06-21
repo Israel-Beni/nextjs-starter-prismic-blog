473b042bd91bb01ad50aed63f34daf62f66d2cad
IsraelBeni <bendegbe72@gmail.com>
2023-06-21 12:23:51 +0530

    completed styling of Header component for all screen sizes

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "@/assets/styles/components/header.module.scss";
 import Navigation from "./Navigation";
 
 import type { ReactChildrenNode } from "@/types";
 
 function Header(): JSX.Element {
     return (
-        <header className="header">
+        <header className={styles.header}>
             {/* @ts-expect-error Server Component */}
             <Navigation />
         </header>
