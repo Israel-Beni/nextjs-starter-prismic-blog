5cbafeb5a3975f85f024d84f7baf9f141eea8e8e
IsraelBeni <bendegbe72@gmail.com>
2023-06-21 11:45:11 +0530

    added reset.scss file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import "@/assets/styles/index.scss";
 import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
 import Header from '@/components/shared/Header';
