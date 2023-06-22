dde0929ceddfa38e8c6c8d1a5c85c09bbe7965d8
IsraelBeni <bendegbe72@gmail.com>
2023-06-22 11:28:29 +0530

    created PrismicLink fassade component

--- facades/index.ts
+++ facades/index.ts
@@ -1,9 +1,11 @@
 import MySliceZone from "./prismicio-react/MySliceZone";
+import MyPrismicText from "./prismicio-react/MyPrismicText";
+import MyPrismicLink from "./prismicio-react/MyPrismicLink";
 
 import { createMyClient } from "./prismicio-client/createMyClient";
 
 // from ./prismicio-react
-export { MySliceZone };
+export { MySliceZone, MyPrismicText, MyPrismicLink };
 
 // from ./prismicio-client
 export { createMyClient };
