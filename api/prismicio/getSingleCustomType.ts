d722b4cef0dd4ca60f1eb166d150ec8f4e4c9cbe
IsraelBeni <bendegbe72@gmail.com>
2023-06-24 11:30:34 +0530

    Done followings: * updated 'getSingleCustomType' * created 'isObject' utility function * completed Author component jsx

--- api/prismicio/getSingleCustomType.ts
+++ api/prismicio/getSingleCustomType.ts
@@ -2,14 +2,14 @@ import 'server-only';
 import { createMyClient } from '@/facades';
 import { SingleTypeDocument } from '@/types';
 
-export const getSingleCustomType = async (uid: "author" | "navigation"): Promise<SingleTypeDocument | null> => {
+export const getSingleCustomType = async (documentType: "author" | "navigation", params?: any): Promise<SingleTypeDocument | null> => {
     const client = createMyClient();
 
     try {
-        const document: SingleTypeDocument = await client.getSingle(uid);
+        const document: SingleTypeDocument = await client.getSingle(documentType, params);
         return document;
     } catch (error) {
-        console.error('error occurred', error);
+        console.error('error occurred:', error);
         return null;
     }
 };
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "@/assets/styles/components/header.module.scss";
 import Navigation from "./Navigation";
+import Author from "./Author";
 
-import type { ReactChildrenNode } from "@/types";
 
 function Header(): JSX.Element {
     return (
@@ -10,6 +10,8 @@ function Header(): JSX.Element {
             <div className={styles.container}>
                 {/* @ts-expect-error Server Component */}
                 <Navigation />
+                {/* @ts-expect-error Server Component */}
+                <Author />
             </div>
         </header>
     )
