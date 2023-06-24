b92362d3233573a97c409b8122ea2f11f2fd6977
IsraelBeni <bendegbe72@gmail.com>
2023-06-24 11:22:09 +0530

    written 'checkObjectStructure' type guard function to check if an object repects the structure of a particular interface

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
 import { AuthorDocument, NavigationDocument, AllDocumentTypes } from "@/prismicio-types";
 import { ReactNode } from "react";
+import { isObject } from "@/utils";
 
 // Interfaces
 interface ReactChildrenNode {
@@ -7,15 +8,43 @@ interface ReactChildrenNode {
 }
 
 
-// Types 
-type SingleTypeDocument = AuthorDocument | NavigationDocument;
+// Types
+type SingleTypeDocument = AuthorDocument | NavigationDocument | null;
 type ReusableTypeDocument = Exclude<AllDocumentTypes, SingleTypeDocument>
 
+
+// Type guard functions
+/**
+ * A type guard function to check if an object respects the structure of a particular interface.
+ * @param obj The object to be checked.
+ * @param structure The structure representing the expected interface.
+ * @returns A boolean indicating whether the object matches the structure.
+ */
+function checkObjectStructure<T>(obj: unknown, structure: T): obj is T {
+  if (typeof obj !== "object" || obj === null) return false;
+
+  for (const key in structure) {
+    if (!(key in obj)) return false;
+
+    const structureValue = structure[key];
+    const objValue = (obj as Record<string, unknown>)[key];
+
+    if (typeof structureValue === "object" && structureValue !== null) {
+      if (!checkObjectStructure(objValue, structureValue)) return false;
+    }
+  }
+
+  return true;
+}
 
 export type {
     // Interfaces
     ReactChildrenNode,
+
     // Types
     SingleTypeDocument,
     ReusableTypeDocument,
+
+    // Type guard functions
+    checkObjectStructure,
 };
