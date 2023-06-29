408e996b8f10b023e0cc58e262efe0f7d16ddfa6
IsraelBeni <bendegbe72@gmail.com>
2023-06-29 19:37:59 +0530

    Dynaicly add 'Line' component to Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import styles from "@/assets/styles/components/header.module.scss";
 import Navigation from "./Navigation";
 import Author from "./Author";
+import Line from "../non-shared/Line";
 
+export interface HeaderProps {
+    params: {
+        slug: string;
+    };
+}
 
-function Header(): JSX.Element {
+function Header({ params }: HeaderProps): JSX.Element {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
@@ -12,9 +18,10 @@ function Header(): JSX.Element {
                 <Navigation />
                 {/* @ts-expect-error Server Component */}
                 <Author />
+                {params.slug && (params.slug === "about" || params.slug === 'contact-me') && <Line />}
             </div>
         </header>
-    )
+    );
 }
 
 export default Header;
