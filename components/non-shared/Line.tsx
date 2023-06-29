e576475262ee6b01094c80578949c7f43add9f68
IsraelBeni <bendegbe72@gmail.com>
2023-06-29 19:19:11 +0530

    created Line component

---
+++ components/non-shared/Line.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '@/assets/styles/components/line.module.scss';
+
+function Line(): JSX.Element {
+    return <hr className={styles.line} />;
+}
+
+export default Line;
